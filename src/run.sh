a18253c982c58bf07131b555d3c2ed71133b8615
Jason Wyse <timeismoneyzyg@163.com>
2014-11-13 23:58:05 +0800

    sh

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 JAVA_HOME/bin/javac $pro/src/ict/edu/learning/logisticRankSVM/LogisticRankSVM.java
 echo 'compiling is over'
-nohup $JAVA_HOME/bin/java ict/edu/learning/logisticRankSVM/LogisticRankSVM -train $pro/data/\
-OHSUMED/OHSUMED/QueryLevelNorm/Fold1/train.txt  -test  $pro/data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/test.txt \
--validate   $pro/data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
+nohup $JAVA_HOME/bin/java ict/edu/learning/logisticRankSVM/LogisticRankSVM -train data/\
+OHSUMED/OHSUMED/QueryLevelNorm/Fold1/train.txt  -test  data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/test.txt \
+-validate   data/OHSUMED/OHSUMED/QueryLevelNorm/Fold1/vali.txt -nThread 17 -norm zscore -learningRate 0.00000001 &
 echo 'It is done'
 
